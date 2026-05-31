b072974234cf84e56e56644e71fae01cf74a9ebc
Paweł Łoćwin <69070227+baguetedev@users.noreply.github.com>
2026-06-01 01:12:44 +0200

    Optymalizacja konfiguracji Sphinxa dla PDF - zmniejszenie pustych stron

--- source/conf.py
+++ source/conf.py
@@ -18,10 +18,37 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+# -- Options for LaTeX output ------------------------------------------------
+# Optymalizacja generowania PDF
+latex_elements = {
+    'papersize': 'a4paper',
+    'pointsize': '11pt',
+    'fncychap': '\\usepackage[Bjarne]{fncychap}',
+    'preamble': r'''
+\usepackage[utf-8]{inputenc}
+\usepackage{babel}
+\usepackage{graphicx}
+\usepackage{hyperref}
+\setcounter{tocdepth}{2}
+\raggedbottom
+''',
+    'sphinxsetup': 'hmargin={0.7in,0.7in}, vmargin={0.7in,0.7in}, verbatimwithframe=false',
+}
+
+latex_documents = [
+    ('index', 'sprawozdanie-z-laboratoriow.tex', 'Sprawozdanie z Laboratorium: Bazy Danych', 
+     'Paweł Łoćwin', 'manual'),
+]
+
+# Zmniejszenie głębi spisu treści w PDF
+latex_show_urls = 'footnote'
+latex_show_pagerefs = False
+
+# Ustawienia dla obrazów w PDF
+latex_max_embed_pages = 0
