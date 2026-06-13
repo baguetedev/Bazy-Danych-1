9dd58ab032a8d0e06b38a95160a736d36b7624c2
Łoćwin Paweł Tomasz <student15@VPS-IP>
2026-06-13 20:40:03 +0200

    Terminator: Globalna aktualizacja treści i submodułów

--- source/conf.py
+++ source/conf.py
@@ -1,7 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+k# Configuration file for the Sphinx documentation builder.
 
 import os
 import sys
@@ -9,15 +6,11 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'Sprawozdanie-z-laboratoriów'
 copyright = '2026, Paweł Łoćwin'
 author = 'Paweł Łoćwin'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 # Dodanie rozszerzenia autodoc
 extensions = [
     'sphinx.ext.autodoc'
@@ -25,3 +18,43 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# -- Options for LaTeX output ------------------------------------------------
+# Optymalizacja generowania PDF - eliminacja pustych stron między rozdziałami
+latex_elements = {
+    'papersize': 'a4paper',
+    'pointsize': '11pt',
+    'fncychap': '\\usepackage[Bjarne]{fncychap}',
+    'extraclassoptions': 'openany,oneside',  # KLUCZOWE: wyłącza openright i twoside
+    'preamble': r'''
+\usepackage{babel}
+\usepackage{graphicx}
+\usepackage{hyperref}
+\setcounter{tocdepth}{2}
+\raggedbottom
+
+% Zmniejszenie przestrzeni przed nagłówkami
+\setlength{\parskip}{0pt plus 1pt}
+\setlength{\parindent}{0pt}
+
+% Zwiększenie wysokości headera
+\setlength{\headheight}{14.49998pt}
+''',
+    'sphinxsetup': 'hmargin={0.7in,0.7in}, vmargin={0.7in,0.7in}, verbatimwithframe=false',
+}
+
+latex_documents = [
+    ('index', 'sprawozdanie-z-laboratoriow.tex', 'Sprawozdanie z Laboratorium: Bazy Danych', 
+     'Paweł Łoćwin', 'manual'),
+]
+
+latex_show_urls = 'footnote'
+latex_show_pagerefs = False
+latex_max_embed_pages = 0
+
+# KLUCZOWE: Wyłącza generowanie bzdurnego "Python Module Index" w PDF i spisie treści
+latex_domain_indices = False
